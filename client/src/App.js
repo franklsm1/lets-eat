94a5e598e991af6f0ae7bc3b00d50243b9a9745f
Sean Franklin <sfrbf@allstate.com>
2021-05-12 23:33:43 -0400

    Move UI into client folder, add server with socket.io along with serverless function

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({clientList}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clients Connected on the same network: <span id="network-clients">{clientList || 'loading...'}</span>
         </p>
         <a
           className="App-link"
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import socketIOClient from 'socket.io-client'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let socket = socketIOClient.connect(window.location.origin);
+
+let clientList;
+socket.on('network-clients', (data) => {
+    clientList = data;
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App clientList />
   </React.StrictMode>,
   document.getElementById('root')
 );
